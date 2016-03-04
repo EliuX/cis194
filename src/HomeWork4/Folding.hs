cbb53ec109717f0e8cdf02c4bec4842e0bc57380
Eliecer Hernandez <eliecerhdz@gmail.com>
2016-03-04 14:27:42 -0500

    Pausing project because of new project

---
+++ src/HomeWork4/Folding.hs
@@ -0,0 +1,22 @@
+module HomeWork4.Folding(fun1, fun2, fun1', fun2') where
+
+-- Multiplies every even number decreased in two
+fun1 :: [Integer] -> Integer
+fun1 []         = 1
+fun1 (x:xs)
+    | even x    = (x - 2) * fun1 xs
+    | otherwise = fun1 xs
+
+fun1' :: [Integer] -> Integer
+fun1' = product . map ((-2)+) . filter even 
+
+-- Given a number 'n' if its even it plus it and apply the function to its half, else case it aplies the function to its triples plus one
+fun2 :: Integer -> Integer
+fun2 1             = 0
+fun2 n | even n    = n + fun2 (n `div` 2)
+       | otherwise = fun2 (3 * n + 1)
+
+fun2' :: Integer -> Integer  
+fun2' = sum . iterate (\x -> if even x then x + (x `div` 2) else  3 * x + 1)       
+
+iterate 
