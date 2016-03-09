f8c1a402a6cb36cc1fd219f71332f543d161b668
Eliecer Hernandez Garbey <eliecerhdz@gmail.com>
2016-03-09 03:05:37 -0500

    HomeWork5 until Execise 5

--- src/HomeWork5/Calc.hs
+++ src/HomeWork5/Calc.hs
@@ -1,8 +1,9 @@
-module HomeWork5.Calc(eval, evalStr, lit, mul, add, reify) where
+module HomeWork5.Calc(eval, evalStr, lit, mul, add, printInt, printBool) where
 
 import HomeWork5.ExprT
+import HomeWork5.MinMax
+import HomeWork5.Mod7
 import HomeWork5.Parser
-
 
 class Calculable a where
  eval :: a -> Integer
@@ -23,6 +24,31 @@ instance Calculable ExprT where
  eval (Mul a b) = (eval a) * (eval b)
  eval (Add a b) = (eval a) + (eval b)
 
+-- Instance of Integer
+instance Expr Integer where
+ lit = id
+ mul x y = x * y
+ add x y = x + y
+
+-- Instance of Bool
+instance Expr Bool where
+ lit x
+    | (x>0)= True
+    | otherwise = False
+ mul x y = x || y
+ add x y = x && y
+
+-- Instance for MinMax
+instance Expr MinMax where
+ lit x = MinMax x
+ mul (MinMax x) (MinMax y) = MinMax (min x y)
+ add (MinMax x) (MinMax y) = MinMax (max x y)
+
+instance Expr Mod7 where
+ lit x = Mod7 (mod x 7)
+ mul (Mod7 x) (Mod7 y) = Mod7 (mod (x * y) 7)
+ add (Mod7 x) (Mod7 y) = Mod7 (mod (x + y) 7)
+
 -- Produces Nothing for inputs which are not well-formed expressions, and
 -- Just n for well-formed inputs that evaluate to n.
 evalStr :: String -> Maybe Integer
@@ -30,6 +56,7 @@ evalStr x = case parseExp Lit Add Mul x of
                 Just a -> Just (eval a)
                 _      -> Nothing
 
-
-reify :: ExprT -> ExprT
-reify = id
+printInt :: Integer -> Integer
+printInt = id
+printBool :: Bool -> Bool
+printBool = id
